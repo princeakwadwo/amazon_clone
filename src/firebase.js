e786e68d7ba82493cec1bf93aa18b3f742807721
princeakwadwo <prince.anniekwadwo@gmail.com>
2021-01-17 02:27:57 +0000

    second checj in with new files

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDm04b17Zl9Xc1xZozKC0DiKc-ahwiOqpQ",
+    authDomain: "clone-1312b.firebaseapp.com",
+    projectId: "clone-1312b",
+    storageBucket: "clone-1312b.appspot.com",
+    messagingSenderId: "270305044454",
+    appId: "1:270305044454:web:6dec328913cfab61991827",
+    measurementId: "G-B4QFJKVXG0"
+  };
