b6c9d428ab07678a71ed545bdf8fbd5373072c84
Prince K. Annie <prince.annie@persol.net>
2021-01-20 17:32:40 +0000

    more features added

--- src/Product.js
+++ src/Product.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import './Product.css'
+import React from "react";
+import "./Product.css";
 
 function Product({ id, title, price, image, rating }) {
-    return (
-        <div className='product'>
-            <div className='product__info'>
-                <p>{title}</p>
-                <p className='product__price'>
-                    <small>$</small>
-                    <strong>{price}</strong>
-                </p>
-                <div className ='product__rating'>
-                {Array(rating)
-                 .fill()
-                .map((_, i) => (
-                 <p>🌟</p>
+  return (
+    <div className="product" key={id}>
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p key={id + i}>🌟</p>
             ))}
-                </div>
-            </div>
-            <img src={image} alt=''/>
-            <button>Add to Basket</button>
         </div>
-    );
+      </div>
+      <img src={image} alt="" />
+      <button>Add to Basket</button>
+    </div>
+  );
 }
 
-export default Product
+export default Product;
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,17 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDm04b17Zl9Xc1xZozKC0DiKc-ahwiOqpQ",
-    authDomain: "clone-1312b.firebaseapp.com",
-    projectId: "clone-1312b",
-    storageBucket: "clone-1312b.appspot.com",
-    messagingSenderId: "270305044454",
-    appId: "1:270305044454:web:6dec328913cfab61991827",
-    measurementId: "G-B4QFJKVXG0"
-  };
+  apiKey: "AIzaSyDm04b17Zl9Xc1xZozKC0DiKc-ahwiOqpQ",
+  authDomain: "clone-1312b.firebaseapp.com",
+  projectId: "clone-1312b",
+  storageBucket: "clone-1312b.appspot.com",
+  messagingSenderId: "270305044454",
+  appId: "1:270305044454:web:6dec328913cfab61991827",
+  measurementId: "G-B4QFJKVXG0",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+export { db, auth };
