0b0a0568fea9dab4e539cce77208cd7aeaff300f
princeakwadwo <prince.anniekwadwo@gmail.com>
2021-01-21 06:44:40 +0000

    more features added

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,11 @@ import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
 
 function Header() {
+  const [{ basket, user }, dispatch] = useStateValue();
+
   return (
     <div className="header">
       <Link to="/">
@@ -35,7 +38,9 @@ function Header() {
         <Link to="/checkout">
           <div className="header_optionBasket">
             <ShoppingBasketIcon />
-            <span className="header_optionTwoBasketCount">0</span>
+            <span className="header_optionTwoBasketCount">
+              {basket?.length}
+            </span>
           </div>
         </Link>
       </div>
