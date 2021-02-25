d536e5582b677ede4e5e0aec5af5055a0c7c5241
Prince Annie Kwadwo <63071468+princeakwadwo@users.noreply.github.com>
2021-02-25 22:48:10 +0000

    updated a feature

--- src/Payment.js
+++ src/Payment.js
@@ -53,10 +53,7 @@ function Payment() {
       .then((result) => {
         if (result.error) {
           // Show error to your customer
-          console.log(
-            "Payment rejected due the following reason:",
-            result.error.message
-          );
+          console.log("Payment rejected due the following reason:", result.error.message);
         } else {
           // The payment succeeded! then save orders in db
           paymentCompleted(result.paymentIntent);
@@ -118,7 +115,7 @@ function Payment() {
             <h3>Review items and delivery</h3>
           </div>
           <div className="payment__items">
-            {basket.map((item) => (
+            {basket.map((item,key) => (
               <CheckoutProduct
                 id={item.id}
                 title={item.title}
